60a5032fce4425bb76b50c8b773265e0ac14d494
Abby <abbigail.abas@gmail.com>
2016-10-20 11:41:42 -0400

    first commit fam

---
+++ server.js
@@ -0,0 +1,30 @@
+// Include our libraries
+var http = require('http');
+var path = require('path');
+var socketio = require('socket.io');
+var express = require('express');
+var router = express();
+var server = http.createServer(router);
+var io = socketio.listen(server);
+// Use router to point requests to the 'client' folder 
+router.use(express.static(path.resolve(__dirname, 'client')));
+// Variables to hold the messages and the sockets
+var messages = [];
+var sockets = [];
+io.on('connection', function (socket) {
+    console.log('a user connected');
+    
+    //Listen for the event from the sensor.html
+    socket.on('temperature', function (temp) {
+        console.log("Temp is" + temp);
+        io.emit('temperature', temp);
+    });
+    socket.on('disconnect', function () {
+        console.log('user disconnected');
+    });
+});
+// Start our server
+server.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function () {
+    var addr = server.address();
+    console.log("Our server is listening at", addr.address + ":" + addr.port);
+});
